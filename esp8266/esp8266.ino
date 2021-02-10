cd2bbed87b81d1086e958f3e2474179ec3281b40
zduka <zduka@outlook.com>
2021-02-10 22:41:24 +0100

    128kbps mp3 tested with neopixels

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,16 +1,21 @@
 #include "AudioFileSourceSD.h"
 #include "AudioGeneratorMP3.h"
 #include "AudioOutputI2SNoDAC.h"
-
+#include <Adafruit_NeoPixel.h>
 
 #include "core.h"
 #include "wifi_setup.h"
+
+#define CS 16
+#define RGB_LED_PIN 5
+
+
+Adafruit_NeoPixel pixels(8, RGB_LED_PIN, NEO_GRB + NEO_KHZ800);
 
 AudioGeneratorMP3 *mp3;
 AudioFileSourceSD *file;
 AudioOutputI2S *out;
 
-#define CS 16
 
 /* ESP8266 PINOUT
 
@@ -62,7 +67,6 @@ void printDirectory(File dir, int numTabs) {
         }
     }
 }
-
 void setup() {
     Core::Setup();
     Core::Connect({SSID1,PASSWORD1, SSID2, PASSWORD2});
@@ -70,10 +74,18 @@ void setup() {
     //Server.on("/command", server::command);
     Core::Server.begin();
 
-    pinMode(2, OUTPUT);
+    pinMode(4, OUTPUT);
     LOG("Setup done.");
 
+    // test neopixel
+    pixels.begin();
+    pixels.clear();
+    pixels.setPixelColor(0, pixels.Color(64,64,64));
+    pixels.show();
+
     printSD();
+
+    
 
     file = new AudioFileSourceSD("000/006.mp3");
     out = new AudioOutputI2SNoDAC();
@@ -84,18 +96,28 @@ void setup() {
 }
 
 
+unsigned long lastMillis = 0;
+uint8_t i = 0;
 
 void loop() {
   //Core::Loop();
   // put your main code here, to run repeatedly:
-  digitalWrite(2, HIGH);
+  digitalWrite(4, HIGH);
   if (mp3->isRunning()) {
     if (!mp3->loop()) {
       mp3->stop(); 
       LOG("MP3 done");
     }
   }
-  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+  unsigned long t = millis();
+  if (t - lastMillis > 1000 / 30) {
+    lastMillis = t;
+    i += 1;
+    pixels.setPixelColor(0, pixels.Color(i,i,i));
+    pixels.show();
+  }
+  
   //delay(5);
   //printSD();
   //delay(900);
