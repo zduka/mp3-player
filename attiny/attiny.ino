524a633db1bcfc4d77af63b4c0af1eeeaaa45505
zduka <zduka@outlook.com>
2021-03-03 23:49:00 +0100

    Rotary encoder working.

--- attiny/attiny.ino
+++ attiny/attiny.ino
@@ -2,6 +2,10 @@
  */
 #include <tinyNeoPixel_Static.h>
 
+
+
+#include "inputs.h"
+
 /** Chip Pinout
 
                -- VDD   GND --
@@ -18,23 +22,72 @@
 
 #define DCDC_PWR 2
 #define NEOPIXEL 11
+#define CTRL_A 13
+#define CTRL_B 10
+#define CTRL_BTN 12
+#define VOL_A 6
+#define VOL_B 5
+#define VOL_BTN 7
+
+RotaryEncoder ctrl{CTRL_A, CTRL_B, 255};
+RotaryEncoder vol{VOL_A, VOL_B, 15};
+
+Button vol_btn{VOL_BTN};
+Button ctrl_btn{CTRL_BTN};
 
 byte pixels[8 * 3];
 tinyNeoPixel leds = tinyNeoPixel(2, NEOPIXEL, NEO_GRB + NEO_KHZ800, pixels);
 
+void vol_changed() {
+    vol.poll();
+}
+
+void vol_btn_changed() {
+    vol_btn.poll();
+}
+
+void ctrl_changed() {
+    ctrl.poll();
+}
+
+void ctrl_btn_changed() {
+    ctrl_btn.poll();
+}
+
 
 void setup() {
+    Serial.swap();
+    Serial.begin(9600);
+    Serial.println("Hello world");
     // enable 3v3 and 5v 
     pinMode(DCDC_PWR, OUTPUT);
     digitalWrite(DCDC_PWR, HIGH);
     
     pinMode(NEOPIXEL,OUTPUT);
+    leds.setPixelColor(0,255,255,255);
+    leds.show();
     delay(1000);
-    leds.setPixelColor(0,0,0,32); // first LED full RED
+    leds.setPixelColor(0,0,0,0); 
     leds.show();                   // LED turns on.
-    
+    vol.setInterrupt(vol_changed);
+    ctrl.setInterrupt(ctrl_changed);
+    vol_btn.setInterrupt(vol_btn_changed);
+    ctrl_btn.setInterrupt(ctrl_btn_changed);
+
 }
 
 void loop() {
-
+    if (vol_btn.changed() && vol_btn.pressed())
+      vol.setValue(0);
+    if (ctrl_btn.changed() && ctrl_btn.pressed()) 
+      ctrl.setValue(0);
+    if (vol.changed() || ctrl.changed()) {
+      leds.setPixelColor(0, vol.value() * 16, ctrl.value(), 0);
+      leds.show();
+      Serial.print("VOL:");
+      Serial.print(vol.value());
+      Serial.print(", CTRL:");
+      Serial.println(ctrl.value());
+      
+    }
 }
---
+++ attiny/inputs.h
@@ -0,0 +1,124 @@
+
+/** A simple button. 
+ */
+class Button {
+public:
+    Button(uint8_t pin, bool internalPullUp = false):
+        pin_{pin} {
+        pinMode(pin, internalPullUp ? INPUT_PULLUP : INPUT);
+        state_.state = digitalRead(pin);
+    }
+
+    void setInterrupt(void (*isr)()) const {
+        attachInterrupt(digitalPinToInterrupt(pin_), isr, CHANGE);    
+    }
+
+    bool changed() const {
+        return state_.changed;
+    }
+
+    bool pressed() const {
+        state_.changed = 0;
+        return ! state_.state; 
+    }
+
+    bool poll() {
+        uint8_t state = digitalRead(pin_);
+        if (state != state_.state) {
+            state_.changed = 1;
+            state_.state = state;
+            return true;
+        } else {
+            return false;
+        }
+    }
+private:
+    uint8_t pin_;
+
+    mutable volatile struct {
+        uint8_t changed : 1;
+        uint8_t state : 1;
+    } state_;
+};
+
+class RotaryEncoder {
+public:
+    RotaryEncoder(uint8_t pinA, uint8_t pinB, uint8_t maxValue, bool internalPullUp = false):
+        a_{pinA},
+        b_{pinB},
+        maxValue_{maxValue} {
+        pinMode(a_, internalPullUp ? INPUT_PULLUP : INPUT);
+        pinMode(b_, internalPullUp ? INPUT_PULLUP : INPUT);
+    }
+
+    void setInterrupt(void (*isr)()) const {
+        attachInterrupt(digitalPinToInterrupt(a_), isr, CHANGE);    
+    }
+
+    /** Updates the value of the encoder.
+
+        Safe to be called from within an ISR.
+     */
+    bool poll() {
+        uint8_t lastState = state_.state;
+        uint8_t state = digitalRead(a_) * 2 + digitalRead(b_);
+        if ((lastState & 2) != (state & 2)) {
+            lastState = ((lastState << 2) + state) & 0xf;
+            state_.state = lastState;
+            switch (lastState) {
+            case 6 : // from 01 to 10
+                return inc();
+            case 3 : // from 00 to 11
+                return dec();
+            }
+        }
+        return false;
+    }
+
+    bool inc() {
+      if (value_ < maxValue_) {
+          ++value_;
+          state_.changed = 1;
+          return true;
+      } else {
+          return false;
+      }
+    }
+
+    bool dec() {
+      if (value_ > 0) {
+          --value_;
+          state_.changed = 1;
+          return true;
+      } else {
+        return false;
+      }
+    }
+
+    bool changed() const {
+        return state_.changed;
+    }
+
+    uint8_t value() const {
+        state_.changed = 0;
+        return value_;
+    }
+
+    void setValue(uint8_t value) {
+        value_ = value;
+        state_.changed = 1;
+    }
+
+private:
+
+    uint8_t a_;
+    uint8_t b_;
+    uint8_t maxValue_;
+    volatile uint8_t value_ = 0;
+
+    mutable volatile struct {
+        uint8_t changed : 1;
+        uint8_t state : 4;
+    } state_;
+  
+};
